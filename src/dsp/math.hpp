bed531831167be78f07a5af46b0229317a15213d
TimeControlledOrganism <47660500+TimeControlledOrganism@users.noreply.github.com>
2019-09-24 13:08:59 +0200

    Add files via upload

--- src/dsp/math.hpp
+++ src/dsp/math.hpp
@@ -70,27 +70,3 @@ inline float sin_01(float t) {
     t = (((-0.540347 * t2 + 2.53566) * t2 - 5.16651) * t2 + 3.14159) * t;
     return t;
 }
-
-inline float MoogVCF(float input, float fc, float res){
-float f = fc * 1.16;
-  float fb = res * (1.0 - 0.15 * f * f);
-  float out1 = 0.0;
-   float out2 = 0.0;
-    float out3 = 0.0;
-	 float out4 = 0.0;
-	   float in1 = 0.0;
-   float in2 = 0.0;
-    float in3 = 0.0;
-	 float in4 = 0.0;
-  input -= out4 * fb;
-  input *= 0.35013 * (f*f)*(f*f);
-  out1 = input + 0.3 * in1 + (1 - f) * out1; // Pole 1
-  in1  = input;
-  out2 = out1 + 0.3 * in2 + (1 - f) * out2;  // Pole 2
-  in2  = out1;
-  out3 = out2 + 0.3 * in3 + (1 - f) * out3;  // Pole 3
-  in3  = out2;
-  out4 = out3 + 0.3 * in4 + (1 - f) * out4;  // Pole 4
-  in4  = out3;
-  return out4;
-}
